a58e4767aa5b60df80f175e11bcde808771fb686
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-28 17:26:56 -0400

    Initial commit for the app files

---
+++ app/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by kshah on 5/28/16.
+ */
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var HelloWorld = react.createClass({
+    render: function () {
+    return (
+            <div>Hello  ReactJS</div>
+        )
+
+}
+});
+
+ReactDOM.render(<Hello React JS/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by kshah on 5/28/16.
+ */
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebPackPluginconfig = new HTMLWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+
+    entry: [
+        './app/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: "index_bundle.js"
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+        ]
+    },
+    plugins: [HTMLWebPackPluginconfig]
+
+}
